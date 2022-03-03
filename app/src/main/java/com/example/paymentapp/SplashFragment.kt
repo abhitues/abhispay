a9964b882faf586166bbc8cf0950d5de770a5942
Abhishek <abhishektues@gmail.com>
2022-03-03 11:39:17 +0530

    Nav Animation and Minor Fix

--- app/src/main/java/com/example/paymentapp/SplashFragment.kt
+++ app/src/main/java/com/example/paymentapp/SplashFragment.kt
@@ -5,30 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.example.paymentapp.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
-    private var _binding: FragmentSplashBinding?= null
+    private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSplashBinding.inflate(inflater,container,false)
-         return binding.root
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnCont.setOnClickListener{
-            findNavController().navigate(R.id.action_splashFragment_to_homeFragment) }
+        binding.btnCont.setOnClickListener {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
     }
 }
